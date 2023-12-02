6bcff1e6231b6399543a0288e2c60fd8ce967249
assistoai <152729796+assistoai@users.noreply.github.com>
2023-12-03 00:08:00 +0530

    Delete chatbot_api copy directory

--- chatbot_api copy/dist/utils/guessApiHost.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const guessApiHost: () => any;
-//# sourceMappingURL=guessApiHost.d.ts.map
--- chatbot_api copy/dist/utils/storage.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const getExistingResultIdFromStorage: (typebotId?: string) => string | undefined;
-export declare const setResultInStorage: (storageType?: 'local' | 'session') => (typebotId: string, resultId: string) => void;
-//# sourceMappingURL=storage.d.ts.map
